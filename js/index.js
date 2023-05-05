a9783117dadc6136aa02aba17ead99314658e33d
Fraustlain <astrocamp29@gmail.com>
2023-05-05 23:49:03 +0800

    changes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     var para = document.querySelectorAll('.parallax');
     var sidenav = document.querySelectorAll('.sidenav');
+    var matbox = document.querySelectorAll('.materialboxed');
+    var car = document.querySelectorAll('.carousel');
+
     M.Parallax.init(para);
     M.Sidenav.init(sidenav);
+    M.Materialbox.init(matbox);
+    M.Carousel.init(car,{
+      fullWidth: true
+    });
+
   });
