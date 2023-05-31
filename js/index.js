9346e08d93db8f352978c706fd4b194725ebe3fc
Fraustlain <astrocamp29@gmail.com>
2023-05-31 23:34:16 +0800

    changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     M.Parallax.init(document.querySelectorAll('.parallax'),{});
     M.Materialbox.init(document.querySelectorAll('.materialboxed'),{});
+    M.FloatingActionButton.init(document.querySelectorAll('.fixed-action-btn'), {});
+    M.Tooltip.init(document.querySelectorAll('.tooltipped'), {});
   });
