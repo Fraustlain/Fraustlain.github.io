781a13aa0da5a1c62679cd5d44773aaf771c70cb
Fraustlain <86923502+Fraustlain@users.noreply.github.com>
2023-07-09 10:57:33 +0800

    asd

---
+++ js/test.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    M.Sidenav.init(document.querySelectorAll('.sidenav'), {});
+    M.Parallax.init(document.querySelectorAll('.parallax'), {
+        responsiveThreshold: 0
+    });
+  });
