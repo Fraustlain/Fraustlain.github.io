d804d069b4da0442dfe20e383582c03f963aacb0
Fraustlain <86923502+Fraustlain@users.noreply.github.com>
2023-09-14 20:12:54 +0800

    bago
    asd

--- js/index.js
+++ js/index.js
@@ -4,3 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
     M.FloatingActionButton.init(document.querySelectorAll('.fixed-action-btn'), {});
     M.Tooltip.init(document.querySelectorAll('.tooltipped'), {});
   });
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
